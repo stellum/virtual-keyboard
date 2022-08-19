4349612bd8098ab9aff5f078813dd578374c9649
stellum <stellaloh16@gmail.com>
2022-08-19 22:42:53 +0900

    feat : keyboard active animation

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -2,6 +2,7 @@ export class Keyboard {
   #switchEl; // Private class fields = class의 속성(property)들은 기본적으로 public하며 class 외부에서 읽히고 수정될 수 있다. 하지만 ES2019 에서는 해쉬 # prefix를 추가해 private class 필드를 선언할 수 있게 되었다.   ;
   #fontSelectEl;
   #containerEl;
+  #keyboardEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -11,11 +12,28 @@ export class Keyboard {
     this.#containerEl = document.getElementById("container");
     this.#switchEl = this.#containerEl.querySelector("#switch");
     this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
   }
 
   #addEvent() {
     this.#switchEl.addEventListener("change", this.#onChangeTheme);
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", (event) => {
+      console.log(event.code);
+      if (this.#keyboardEl.querySelector(`[data-code=${event.code}]`)) {
+        this.#keyboardEl
+          .querySelector(`[data-code=${event.code}]`)
+          ?.classList.add("active");
+      }
+    });
+    document.addEventListener("keyup", (event) => {
+      // console.log("keyup");
+      if (this.#keyboardEl.querySelector(`[data-code=${event.code}]`)) {
+        this.#keyboardEl
+          .querySelector(`[data-code=${event.code}]`)
+          ?.classList.remove("active");
+      }
+    });
   }
 
   #onChangeTheme(event) {
